25eefc7d98c1235f9471e67119883a4d016f01a0
Jakob Schweter <kalp@Jakobs-MBP.lan>
2023-08-16 17:26:40 -0500

    set up basic implmentation for backend and db

---
+++ bookem-backend/migrations/001_create_my_table.py
@@ -0,0 +1,18 @@
+steps = [
+    [
+       ## Create the table
+        """
+        CREATE TABLE appointments (
+            id SERIAL PRIMARY KEY NOT NULL,
+            name VARCHAR(1000) NOT NULL,
+            date TIMESTAMP(0) WITH TIME ZONE
+            description TEXT NOT NULL,
+            price SMALLINT NOT NULL
+        );
+        """,
+       ## Drop the table
+       """
+       DROP TABLE appointments;
+       """
+    ]
+]
